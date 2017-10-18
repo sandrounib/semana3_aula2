682759701485e05e3743e0d791609a93759d8f3e
FIC <sandrounib@hotmail.com>
2017-10-18 18:16:36 -0200

    CPF VALIDATION

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace semana3_aula2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
